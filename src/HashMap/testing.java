66b90596b9600cce5f4956f7f3cd5ec11e1a85de
Alex Mendoza <you@example.com>
2024-11-30 21:27:09 -0600

    Revert "getter setters and toString"
    This reverts commit 5efaff38f1e699ca50367fa339e94ae1027e9f00.

--- src/HashMap/testing.java
+++ src/HashMap/testing.java
@@ -1,27 +1,4 @@
 package HashMap;
 
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class testing {
-    List<Integer> list;
-    public testing () {
-        list =  new ArrayList<>();
-    }
-
-    public List<Integer> getList() {
-        return list;
-    }
-
-    public void setList(List<Integer> list) {
-        this.list = list;
-    }
-
-    @Override
-    public String toString() {
-        return "testing{" +
-                "list=" + list +
-                '}';
-    }
 }
